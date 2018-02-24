abda371063a2dadde99806e1020a831ffc21f759
Gabriel Robert <g.robert092@gmail.com>
2018-02-24 14:06:54 -0500

    Tests enhancement

--- actions_test.go
+++ actions_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 )
 
-func TestCurrent(t *testing.T) {
+func Test_ShouldReturnVersion(t *testing.T) {
 	currentVersion := getCurrentVersion("test/Info.plist")
-	if currentVersion != "1.0.1" {
+	if currentVersion.Version != "1.0.1" {
 		t.Errorf("version mismatch; actual %v, expected %v", currentVersion, "1.0.1")
 	}
 }
--- finders_test.go
+++ finders_test.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"log"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func Test_ShouldFindiOSFile(t *testing.T) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	foundFiles := findiOSFile(dir)
+	foundFiles := findiOSFile("./")
 	if foundFiles == nil || len(foundFiles) == 0 {
 		t.Errorf("test file info.plist has not been found, expected %v", "test/Info.plist")
 	}
