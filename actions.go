34d2a7c07e3cc7ca136dd3307a3db12f2b69fcc3
Gabriel Robert <g.robert092@gmail.com>
2018-02-24 12:36:16 -0500

    plist file can be read and parsed

--- actions.go
+++ actions.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/DHowett/go-plist"
 	"github.com/urfave/cli"
 )
 
+type iOSBundlerHeader struct {
+	BundleVersion string `plist:"CFBundleVersion"`
+}
+
 func current(c *cli.Context) error {
-	fmt.Printf("current version is %v", getCurrentVersion())
+	fmt.Printf("iOS version - %v", getCurrentVersion("test/Info.plist"))
 	return nil
 }
 
-func getCurrentVersion() string {
-	return "1.0.1"
+func getCurrentVersion(filePath string) string {
+	file := openFile(filePath)
+	decoder := plist.NewDecoder(file)
+	var data iOSBundlerHeader
+	err := decoder.Decode(&data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data.BundleVersion
+}
+
+func openFile(filePath string) *os.File {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return file
 }
--- actions_test.go
+++ actions_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestCurrent(t *testing.T) {
-	currentVersion := getCurrentVersion()
+	currentVersion := getCurrentVersion("test/Info.plist")
 	if currentVersion != "1.0.1" {
 		t.Errorf("version mismatch; actual %v, expected %v", currentVersion, "1.0.1")
 	}
