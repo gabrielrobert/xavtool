5c8fda337d1cd8ef8d98a4bfc75b71b1891e75b0
Gabriel Robert <g.robert092@gmail.com>
2018-02-24 17:01:16 -0500

    openFile method unit test

--- actions_test.go
+++ actions_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"testing"
+)
+
+func Test_openFile(t *testing.T) {
+	filepath := "test/Info.plist"
+	file := openFile(filepath)
+	if file == nil {
+		t.Errorf("file %v should be able to be open", filepath)
+	}
+
+	stats, error := file.Stat()
+	if error != nil {
+		t.Errorf("file %v statistics should be available", filepath)
+	}
+
+	size := stats.Size()
+	if size <= 0 {
+		t.Errorf("file %v should not be empty", filepath)
+	}
+}
