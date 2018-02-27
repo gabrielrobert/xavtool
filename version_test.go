9b8710128f8e21b3217dcac3c0ad62fd2b692a9a
Gabriel Robert <g.robert092@gmail.com>
2018-02-27 13:50:28 -0500

    version unit tests

--- version_test.go
+++ version_test.go
@@ -1,1 +1,69 @@
 package main
+
+import "testing"
+
+func Test_incrementMajor(t *testing.T) {
+	type args struct {
+		version string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1.0.0", args{"1.0.0"}, "2.0.0"},
+		{"2.2.1", args{"2.2.1"}, "3.0.0"},
+		{"0.0.0", args{"0.0.0"}, "1.0.0"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := incrementMajor(tt.args.version); got != tt.want {
+				t.Errorf("incrementMajor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_incrementMinor(t *testing.T) {
+	type args struct {
+		version string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1.0.0", args{"1.0.0"}, "1.1.0"},
+		{"2.2.1", args{"2.2.1"}, "2.3.0"},
+		{"0.0.0", args{"0.0.0"}, "0.1.0"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := incrementMinor(tt.args.version); got != tt.want {
+				t.Errorf("incrementMinor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_incrementPatch(t *testing.T) {
+	type args struct {
+		version string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"1.0.0", args{"1.0.0"}, "1.0.1"},
+		{"2.2.1", args{"2.2.1"}, "2.2.2"},
+		{"0.0.0", args{"0.0.0"}, "0.0.1"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := incrementPatch(tt.args.version); got != tt.want {
+				t.Errorf("incrementPatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
