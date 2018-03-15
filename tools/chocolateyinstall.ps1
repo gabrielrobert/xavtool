92167fa54c4b2a8c88fe8dda84ab5e36f6ba1ba9
Gabriel Robert <g.robert092@gmail.com>
2018-03-15 13:14:39 -0400

    Chocolatey initial support

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$distDir = (get-item $toolsDir ).parent.FullName
+
+$fileLocation = Join-Path $distDir 'dist/windows_386/xavtool.exe'
+$fileLocation64 = Join-Path $distDir 'dist/windows_amd64/xavtool.exe'
+
+Write-Host $fileLocation
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+
+  fileType      = 'exe'
+  file           = $fileLocation
+  file64      = $fileLocation64
+
+  softwareName  = 'xavtool*'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
